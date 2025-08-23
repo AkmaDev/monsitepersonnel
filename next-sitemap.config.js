a364e7fcae9e2bf9918e4b1d47997792a8d5b03e
AkmaDev <manews193@gmail.com>
2025-08-23 03:05:46 +0100

    Troisième Mise à jour

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.manasseakpovi.com", // Ton URL principale
+  generateRobotsTxt: true, // Génère aussi un fichier robots.txt
+  changefreq: "daily", // Fréquence de mise à jour (optionnel)
+  priority: 0.7, // Priorité par défaut (optionnel)
+};
